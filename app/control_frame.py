648efdca9471e31bd4b2d363595dd00f546fb13c
Illarionov.A <nuvorish81@gmail.com>
2023-03-12 15:32:51 +0600

    Set adaptive grid in control window

--- app/control_frame.py
+++ app/control_frame.py
@@ -43,22 +43,41 @@ class ControlPanel:
 	def create_frame(self):
 		self.frame = Frame(self.master)
 
-		# for r in range(8): self.frame.rowconfigure(index=r, weight=1)
+		for r in range(9): self.frame.rowconfigure(index=r, weight=1)
 		for c in range(3): self.frame.columnconfigure(index=c, weight=1)
 
-		entry_of_spectators_window = ttk.Entry(self.frame, textvariable=self.title).grid(row=0, column=0, columnspan=3)
-		entry_stage=ttk.Entry(self.frame, textvariable=self.stage).grid(row=1, column=0)
-		entry_group=ttk.Entry(self.frame, textvariable=self.group).grid(row=1, column=1)
-		entry_explanation=ttk.Entry(self.frame, textvariable=self.explanation).grid(row=1, column=2)
+		title = ttk.Entry(self.frame, textvariable=self.title, justify='center')
 
-		entry_name_1 = ttk.Entry(self.frame, textvariable=self.name_1).grid(row=2, column=0)
-		entry_club_1 = ttk.Entry(self.frame, textvariable=self.club_1).grid(row=3, column=0)
-		entry_fall_1 = ttk.Entry(self.frame, textvariable=self.fall_1).grid(row=2, column=1)
-		entry_adv_1 = ttk.Entry(self.frame, textvariable=self.adv_1).grid(row=3, column=1)
-		entry_points_1 = ttk.Entry(self.frame, textvariable=self.points_1).grid(row=2, column=2, rowspan=2)
+		stage=ttk.Entry(self.frame, textvariable=self.stage)
+		group=ttk.Entry(self.frame, textvariable=self.group)
+		explanation=ttk.Entry(self.frame, textvariable=self.explanation)
 
-		entry_name_2 = ttk.Entry(self.frame, textvariable=self.name_2).grid(row=6, column=0)
-		entry_club_2 = ttk.Entry(self.frame, textvariable=self.club_2).grid(row=7, column=0)
-		entry_fall_2 = ttk.Entry(self.frame, textvariable=self.fall_2).grid(row=6, column=1)
-		entry_adv_2 = ttk.Entry(self.frame, textvariable=self.adv_2).grid(row=7, column=1)
-		entry_points_2 = ttk.Entry(self.frame, textvariable=self.points_2).grid(row=6, column=2, rowspan=2)
+		name_1 = ttk.Entry(self.frame, textvariable=self.name_1)
+		club_1 = ttk.Entry(self.frame, textvariable=self.club_1)
+		fall_1 = ttk.Entry(self.frame, textvariable=self.fall_1)
+		adv_1 = ttk.Entry(self.frame, textvariable=self.adv_1)
+		points_1 = ttk.Entry(self.frame, textvariable=self.points_1)
+
+		name_2 = ttk.Entry(self.frame, textvariable=self.name_2)
+		club_2 = ttk.Entry(self.frame, textvariable=self.club_2)
+		fall_2 = ttk.Entry(self.frame, textvariable=self.fall_2)
+		adv_2 = ttk.Entry(self.frame, textvariable=self.adv_2)
+		points_2 = ttk.Entry(self.frame, textvariable=self.points_2)
+
+		title.grid(row=0, column=0, columnspan=3, sticky='nsew')
+
+		name_1.grid(row=2, column=0, sticky='nsew')
+		club_1.grid(row=3, column=0, sticky='nsew')
+		fall_1.grid(row=2, column=1, sticky='nsew')
+		adv_1.grid(row=3, column=1, sticky='nsew')
+		points_1.grid(row=2, column=2, rowspan=2, sticky='nsew')
+
+		name_2.grid(row=5, column=0, sticky='nsew')
+		club_2.grid(row=6, column=0, sticky='nsew')
+		fall_2.grid(row=5, column=1, sticky='nsew')
+		adv_2.grid(row=6, column=1, sticky='nsew')
+		points_2.grid(row=5, column=2, rowspan=2, sticky='nsew')
+
+		stage.grid(row=8, column=0, sticky='nsew')
+		group.grid(row=8, column=1, sticky='nsew')
+		explanation.grid(row=8, column=2, sticky='nsew')
